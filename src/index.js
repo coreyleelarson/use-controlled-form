63d074931c6d7e62e7a6dd8db107a31d27010c90
Corey Larson <coreyleelarson@gmail.com>
2019-02-20 13:21:53 -0600

    Adds comments and cleans up file.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import { useState } from 'react';
 export default ({ initialValues = {}, onSubmit: handleSubmit }) => {
   const [values, setValues] = useState(initialValues);
 
-  const onChange = e => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-
-  const onSubmit = e => {
-    e.preventDefault();
-    if (typeof handleSubmit === 'function') {
-      handleSubmit({ values });
-    }
-  }
-
+  // field props object to be passed to <input />
   const fields = Object.keys(values)
     .reduce((obj, key) => {
       obj[key] = {
@@ -24,6 +13,20 @@ export default ({ initialValues = {}, onSubmit: handleSubmit }) => {
       };
       return obj;
     }, {});
+
+  // onChange callback to be passed to <input />
+  function onChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  // onSubmit callback to be passed to <form />
+  function onSubmit(e) {
+    e.preventDefault();
+    if (typeof handleSubmit === 'function') {
+      handleSubmit({ values });
+    }
+  }
 
   return { fields, onSubmit };
 }
